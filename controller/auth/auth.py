110f367ff18b888be5a1322fa0485fedd90f5a01
danieltonad <soladan2000@gmail.com>
2024-05-16 03:52:22 -0900

    feat: auth completed

--- controller/auth/auth.py
+++ controller/auth/auth.py
@@ -10,18 +10,18 @@ async def login_user_controller(username: str, password:str):
         user = users_db.find_one({'username': username, 'password': hash_pwd(password)})
         if user:
             token = await generate_access_token(user)
-            return token
+            return AppResponse(message="login successfull!", status_code=status.HTTP_200_OK, data=token)
         else:
-            return {'detail': 'invalid user credentials'}
-    except:
-        return AppResponse(message="Unable to register user", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return AppResponse(message="incorrect user credentials!", status_code=status.HTTP_403_FORBIDDEN)
+    except Exception as e:
+        return AppResponse(message="Unable to login!", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
 
 async def register_user_controller(username: str, password: str):
     try:
-        user = users_db.insert_one({'username': username, 'password': password})
-        return user
+        users_db.insert_one({'username': username, 'password': hash_pwd(password)})
+        return AppResponse(message="User account created", status_code=status.HTTP_200_OK)
     except Exception as e:
         print(e)
-        return {'status': status.HTTP_500_INTERNAL_SERVER_ERROR, 'message': 'user created!'}
+        return AppResponse(message="Unable to create account!", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     

582181309dc4474e79cd654a570da398ad07eb39
danieltonad <soladan2000@gmail.com>
2024-05-17 04:01:55 -0900

    feat: task --sort

--- schema/tasks.py
+++ schema/tasks.py
@@ -1,15 +1,14 @@
 from bson import ObjectId
-from datetime import date
+from datetime import datetime
 
 def task_serializer(task: dict) -> dict:
-    print(task.get('created_at'))
     return {
         'id': str(ObjectId(task.get('_id'))),
         'title': task.get('title'),
         'description': task.get('description') or "",
         'completed': task.get('completed'),
-        'created_at': str(task.get('created_at')),
-        'updated_at': strdate(task.get('updated_at'))
+        'created_at': task.get('created_at').strftime('%d-%m-%Y %H:%M'),
+        'updated_at': task.get('updated_at').strftime('%d-%m-%Y %H:%M')
     }
     
 def tasks_serializer(tasks: list) -> list:
